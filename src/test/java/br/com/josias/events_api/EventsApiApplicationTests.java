93e5304f555143609bbca73af44afe16fab5ad28
josiassantos15 <josiassantos1577@gmail.com>
2025-02-22 19:19:45 -0300

    first commit

---
+++ src/test/java/br/com/josias/events_api/EventsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.josias.events_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
